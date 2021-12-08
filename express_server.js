ae29825b31b48f3973a072efc73e013331893d26
somar7 <somar7@nait.ca>
2021-12-08 23:42:10 +0000

    URL Shortening

--- express_server.js
+++ express_server.js
@@ -4,10 +4,44 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/u/:shortURL", (req, res) => {
+  //const longURL =
+  longURL = urlDatabase[req.params.shortURL]
+   console.log(longURL);
+   res.redirect(longURL);
+ });
+  
+
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: long };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  let code = generateRandomString(6)
+  res.redirect(`http://localhost:8080/urls/${code}`);         // Respond with 'Ok' (we will replace this)
+   urlDatabase[code] = req.body.longURL;
+   console.log(urlDatabase);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,27 +59,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: long };
-  res.render("urls_show", templateVars);
-});
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
 function generateRandomString(length) {
   let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
@@ -54,5 +67,5 @@ function generateRandomString(length) {
     answer += char.charAt((Math.random()*char.length));
   }
   return answer;
-} 
+};
 
